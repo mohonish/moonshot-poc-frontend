b475ccb2c2f41d2fbc18daaad287171a3838e248
mohonish <mohonish.c@gmail.com>
2020-05-30 17:22:13 -0400

    added amcharts with example implementation in weather

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,8 +1,65 @@
 <template>
-  <div class="about">
-    <h3>ACDM Experiment <a href="https://mse-moonshot.atlassian.net/browse/MS-219">MS-219</a> to show feasibility of chosen tech stack.</h3>
+  <div class="weather">
+    <h2>Weather Data</h2>
+    <div class="worldmap" ref="chartdiv">
+    </div>
   </div>
 </template>
 
+<script>
+import * as am4core from '@amcharts/amcharts4/core'
+import * as am4charts from '@amcharts/amcharts4/charts'
+// eslint-disable-next-line camelcase
+import am4themes_animated from '@amcharts/amcharts4/themes/animated'
+
+am4core.useTheme(am4themes_animated)
+export default {
+  name: 'WorldMap',
+  mounted () {
+    const chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart)
+
+    chart.paddingRight = 20
+
+    const data = []
+    let visits = 10
+    for (let i = 1; i < 366; i++) {
+      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10)
+      data.push({ date: new Date(2018, 0, i), name: 'name' + i, value: visits })
+    }
+
+    chart.data = data
+
+    const dateAxis = chart.xAxes.push(new am4charts.DateAxis())
+    dateAxis.renderer.grid.template.location = 0
+
+    const valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
+    valueAxis.tooltip.disabled = true
+    valueAxis.renderer.minWidth = 35
+
+    const series = chart.series.push(new am4charts.LineSeries())
+    series.dataFields.dateX = 'date'
+    series.dataFields.valueY = 'value'
+
+    series.tooltipText = '{valueY.value}'
+    chart.cursor = new am4charts.XYCursor()
+
+    const scrollbarX = new am4charts.XYChartScrollbar()
+    scrollbarX.series.push(series)
+    chart.scrollbarX = scrollbarX
+
+    this.chart = chart
+  },
+  beforeDestroy () {
+    if (this.chart) {
+      this.chart.dispose()
+    }
+  }
+}
+</script>
+
 <style scoped>
+  #chartdiv {
+    width: 100%;
+    height: 500px;
+  }
 </style>
